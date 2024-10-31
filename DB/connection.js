446dd25b2e7da8e7ff0caa04b0f17c805721b753
Syed Kumail Raza <136350543+syedkumailraza1@users.noreply.github.com>
2024-10-31 13:30:51 +0530

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const ConnectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}?retryWrites=true&w=majority`)
         console.log(`MongoDB connected: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log('Error while DB connection', error)
